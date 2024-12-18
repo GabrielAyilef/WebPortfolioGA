9f3b0a442162db3f4056e720b044c32c12119242
GabrielAyilef <159633354+GabrielAyilef@users.noreply.github.com>
2024-12-18 11:42:03 +0100

    managing Main thorugh CSS queries

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -6,37 +6,42 @@ import Contact from './contact/Contact'
 import Footer from "./footer/Footer"
 import { Outlet } from "react-router-dom";
 import { useDarkMode } from './projects/Context/DarkMode'
-import { useMobile } from './projects/Context/Mobile'
+
 
 
 function Main({ className }) {
     const { darkPortfolio } = useDarkMode()
-    const {isMobile} = useMobile()
-
-  function handleClick(e) {
-    if (isMobile) {
-
-      e.stopPropagation();
-    }
-}
- 
-   
-    return (
-        <div className={`${className} ${darkPortfolio ? "dark" : ""} ${isMobile ? "mobile" : ""}`}>
-          <div className={` project-wrapper-scale ${isMobile ? "mobile" : ""}`}>
-            <Profile />
-            <About />
-            <div id="project">
-              <Outlet />
-            </div>
-            <Contact />
-            <Footer />
+    
+return (
+      <div className={`${className} ${darkPortfolio ? "dark" : ""} `}>
+        <div className="project-wrapper-scale" >
+          <Profile />
+          <About />
+          <div id="project">
+            <Outlet />
+          </div>
+          <Contact />
+          <Footer />
         </div>
-        {isMobile && (
-        <div className="mobile-overlay" />
-      )}
-        </div>
-       );
+      </div>
+    );
 }
 
 export default Main
+
+// return (
+//   <div className={`${className} ${darkPortfolio ? "dark" : ""} ${isMobile ? "mobile" : ""}`}>
+//     <div className={` project-wrapper-scale ${isMobile ? "mobile" : ""}`}>
+//       <Profile />
+//       <About />
+//       <div id="project">
+//         <Outlet />
+//       </div>
+//       <Contact />
+//       <Footer />
+//   </div>
+//   {isMobile && (
+//   <div className="mobile-overlay" />
+// )}
+//   </div>
+//  );
